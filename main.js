771684bc8023fbcbad0004322cc5a2ebbea16f6e
Anju Dhungana <anjudhungana28@gmail.com>
2022-01-27 09:19:25 -0500

    owl carosuel added for portfoli display

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
+//owlcarousel
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        autoplay:true,
+        autoplayhoverpause:true,
+        autoplaytimeout:100,
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        center:true,
+        nav:true,
+        navText:[
+          " <i class='fa fa-angle-left'></i>",
+          " <i class='fa fa-angle-right'></i>"
+        ],
+        responsive:{
+            0:{
+                items:1,
+               
+               
+            },
+            600:{
+                items:2,
+                
+            },
+            1000:{
+                items:3,
+               
+            }
         }
-    }
-})
+    })
+    });
