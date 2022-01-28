2d38f3ab85abad89d33bc12108d130919f8d94b6
Anju Dhungana <anjudhungana28@gmail.com>
2022-01-28 11:31:15 -0500

    created seperate files for each css

--- main.js
+++ main.js
@@ -2,17 +2,24 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         autoplay:true,
-        autoplayhoverpause:true,
-        autoplaytimeout:100,
+
         loop:true,
+
         margin:10,
+
         responsiveClass:true,
+
         center:true,
+
         nav:false,
+
         navText:[
           " <i class='fa fa-angle-left'></i>",
           " <i class='fa fa-angle-right'></i>"
         ],
+
+
+        //shows items according to display size
         responsive:{
             0:{
                 items:1,
@@ -44,11 +51,4 @@ $(document).ready(function(){
             $('#back2Top').fadeOut();
         }
     });
-    $(document).ready(function() {
-        $("#back2Top").click(function(event) {
-            event.preventDefault();
-            $("html, body").animate({ scrollTop: 0 }, "slow");
-            return false;
-        });
     
-    });
